683cabb86e550997b34cbeb43899a541070f9ac3
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-22 19:41:38 +0800

    Update AMM.sol

--- AMM.sol
+++ AMM.sol
@@ -86,8 +86,8 @@ contract AMM is AccessControl{
 	function provideLiquidity( uint256 amtA, uint256 amtB ) public {
 		require( amtA > 0 || amtB > 0, 'Cannot provide 0 liquidity' );
 		//YOUR CODE HERE
-    ERC20(tokenA).transferFrom(msg.sender, address(this), amtA);
-    ERC20(tokenB).transferFrom(msg.sender, address(this), amtB);
+		//ERC20(tokenA).transferFrom(msg.sender, address(this), amtA);
+		//ERC20(tokenB).transferFrom(msg.sender, address(this), amtB);
 
     invariant = ERC20(tokenA).balanceOf(address(this))*ERC20(tokenB).balanceOf(address(this));
 
