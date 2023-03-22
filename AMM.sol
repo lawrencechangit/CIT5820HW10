3181d5b60d2f8ca13682629d8a3b2d0a74a13f95
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-03-22 20:18:13 +0800

    Update AMM.sol

--- AMM.sol
+++ AMM.sol
@@ -63,7 +63,8 @@ contract AMM is AccessControl{
       ERC20(tokenA).transferFrom(msg.sender, address(this), qtyA);
       swapAmt=qtyA*(10**4-feebps)/10**4;
       uint256 denominator=ERC20(tokenA).balanceOf(address(this))+swapAmt;
-      qtyB=ERC20(tokenB).balanceOf(address(this))-invariant/denominator;
+      fraction=invariant/denominator;
+      qtyB=ERC20(tokenB).balanceOf(address(this))-fraction;
       ERC20(tokenB).transferFrom(address(this), msg.sender, qtyB);
 
       emit Swap( address(this), msg.sender, qtyA, qtyB );
@@ -74,7 +75,8 @@ contract AMM is AccessControl{
       ERC20(tokenB).transferFrom(msg.sender, address(this), qtyB);
       swapAmt=qtyB*(10**4-feebps)/10**4;
       uint256 denominator=ERC20(tokenB).balanceOf(address(this))+swapAmt;
-      qtyA=ERC20(tokenA).balanceOf(address(this))-invariant/denominator;
+      fraction=invariant/denominator;
+      qtyA=ERC20(tokenA).balanceOf(address(this))-fraction;
       ERC20(tokenA).transferFrom(address(this), msg.sender, qtyA);
 
       emit Swap( address(this), msg.sender, qtyB, qtyA );
